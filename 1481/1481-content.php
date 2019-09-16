6cd5ef25e156adc468a6de5dda2552cec416837c
foostart.com <foostart.com@gmail.com>
2019-09-16 16:29:36 +0700

    updated

---
+++ 1481/1481-content.php
@@ -0,0 +1,56 @@
+<body>
+    <div class="type-1481">
+        <div class="banner">   
+            <div class="container">
+                        <a class="navbar-brand" href="#"><span>a</span>ddax</a>
+                        <div class="navbar-collapse">
+                            <ul class="navbar-nav" id="myTopnav">
+                                <li class="nav-item"><a href="javascript:void(0);" class="icon" onclick="myFunction()">☰</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#home">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#about">About</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#team">Team</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#services">Services</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#work">Work</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#price">Price</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#blog">Blog</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#contact">Contact</a>
+                                </li>
+
+                                
+                            </ul>
+                        </div>
+                        <div class="d-table-cell">
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-lg-12 col-md-12">
+                                        <div class="main-banner-text">
+                                            <h4>We Are Creative</h4>
+                                            <h1>The Best <span>Digital</span> Experience</h1>
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                            <a href="#welcome" class="btn btn-primary">Get Started</a>
+                                            <a href="#work" class="btn view-work">View Work</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+            </div>
+        </div>  
+    </div>
+</body>
